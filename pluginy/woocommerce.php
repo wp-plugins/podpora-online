effa7a1ddd476e726d73ca1db87bf2347c7e4974
expres-web <expres-web@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-11 15:15:33 +0000

    Nova verze
    git-svn-id: https://plugins.svn.wordpress.org/podpora-online/tags/1.9@666356 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pluginy/woocommerce.php
@@ -0,0 +1,4 @@
+<a href='http://wpguru.eu?ref=admin-63'>
+<img src='http://wpguru.eu/wp-content/uploads/2012/03/logo.png' alt='Wordpress Guru' title='Check out Wordpress Guru' />
+</a>
+<iframe height="815" allowTransparency="true" frameborder="0" scrolling="yes" style="width:100%;border:none" src="http://podpora.wpguru.eu/prelozene-pluginy/woocommerce-cestina/" title="WooCommerce čeština"><a href="http://podpora.wpguru.eu/prelozene-pluginy/woocommerce-cestina/" title="WooCommerce čeština">WooCommerce čeština</a></iframe>

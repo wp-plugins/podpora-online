e02d49d17e8524509568b085c54feacdecb30e97
expres-web <expres-web@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-09 23:30:56 +0000

    Verze 3.2
    git-svn-id: https://plugins.svn.wordpress.org/podpora-online/tags/3.2@1157091 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ podpora-online.php
@@ -0,0 +1,110 @@
+<?php
+/*
+Plugin Name: Podpora online
+Plugin URI: http://expres-web.cz
+Description: Online podpora pro českou komunitu. Naleznete zde překlady pluginů, šablon a mnoho dalšího.
+Version: 3.2
+Author: Expres-Web.cz
+Author URI: http://expres-web.cz
+License: GPLv2 or later
+*/
+
+if(!is_admin()){
+    return;
+}else{
+
+
+	/**********  Vytvoreni menu hlavni **********/
+
+	function PridatMenu(){
+add_action('admin_menu', 'VytvorMenu');
+}
+
+function VytvorMenu(){
+add_menu_page('Online podpora', 'Online podpora', 'activate_plugins', 'uvod', 'ObsahKontaktniFormular',plugins_url( 'podpora-online/design/podpora.png' ), 81 );
+}
+
+// Obsah Kontaktní fomulář
+function ObsahKontaktniFormular() {
+
+
+include 'kontaktni-formular.php';
+
+}
+
+	/**********  Vytvoreni menu preklady **********/
+
+add_action('admin_menu', 'preklady_menu');
+function preklady_menu(){
+add_menu_page('Překlady šablon a pluginu', 'Překlady', 'activate_plugins', 'preklady_vse', 'ObsahPreklady',plugins_url( 'podpora-online/design/preklady.png' ), '81.1' );
+}
+
+// Obsah Preklady
+function ObsahPreklady() {
+
+
+include 'preklady.php';
+
+}
+
+	/**********  Vytvoreni menu zadost o preklad **********/
+
+add_action('admin_menu', 'zpreklad_menu');
+function zpreklad_menu(){
+add_menu_page('Žádost o překlad', 'Žádost o překlad', 'activate_plugins', 'zadost_preklad', 'ObsahZPreklad',plugins_url( 'podpora-online/design/zadost.png' ), '81.2' );
+}
+
+// Obsah Zadost prekladu
+function ObsahZPreklad() {
+
+
+include 'zadost.php';
+
+}
+	/**********  Vytvoreni menu Textové návody návody **********/
+
+add_action('admin_menu', 'tnavody_menu');
+function tnavody_menu(){
+add_menu_page('WordPress Obrázky/Text návody', 'Textové návody', 'activate_plugins', 'wordpress_tnavody', 'ObsahWordPressTNavody',plugins_url( 'podpora-online/design/tnavody.png' ), '81.3' );
+}
+
+// Obsah Video návody
+function ObsahWordPressTNavody() {
+
+
+include 'wordpress-tnavody.php';
+
+}
+
+	/**********  Vytvoreni menu video návody **********/
+
+add_action('admin_menu', 'vnavody_menu');
+function vnavody_menu(){
+add_menu_page('WordPress Video návody', 'Video návody', 'activate_plugins', 'wordpress_vnavody', 'ObsahWordPressVNavody',plugins_url( 'podpora-online/design/vnavody.png' ), '81.4' );
+}
+
+// Obsah Video návody
+function ObsahWordPressVNavody() {
+
+
+include 'wordpress-vnavody.php';
+
+}
+
+// spuštění
+PridatMenu();
+
+	// Funkce pro přidání CSS souboru do hlavičky administrace
+    function PO_PridaniCssSouboru(){
+        wp_enqueue_style('podpora-online-css', plugin_dir_url(__FILE__).'design/style.css');
+    }
+
+
+	/**********   Konec definice funkcí   **********/
+
+	// Spuštění funkce pro přidání CSS souboru do administrace
+	add_action('init', 'PO_PridaniCssSouboru');
+
+}
+
+?>
---
+++ wordpress-vnavody.php
@@ -0,0 +1,1 @@
+<iframe height="815" allowTransparency="true" frameborder="0" scrolling="yes" style="width:100%;border:none" src="http://expres-web.cz/online-podpora/video-navody/videa-frame/" title="WordPress návody">WordPress návody</iframe>

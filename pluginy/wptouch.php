b37b3eeff0dfbb5d3bc6c28dce136d95bcd9f6fd
expres-web <expres-web@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-15 21:09:47 +0000

    Ver 3.1.1
    git-svn-id: https://plugins.svn.wordpress.org/podpora-online/branches/3.0@1161486 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pluginy/wptouch.php
@@ -0,0 +1,1 @@
+<iframe height="815" allowTransparency="true" frameborder="0" scrolling="yes" style="width:100%;border:none" src="http://expres-web.cz/online-podpora/prelozene-pluginy/wptouch-cestina/" title="WPtouch čeština">WPtouch čeština</iframe>
---
+++ sablony/multinews.php
@@ -0,0 +1,1 @@
+<iframe height="815" allowTransparency="true" frameborder="0" scrolling="yes" style="width:100%;border:none" src="http://expres-web.cz/online-podpora/prelozene-sablony/multinews-multi-purpose-wordpress-newsmagazine/" title="Multinews šablona">Multinews šablona</iframe>
